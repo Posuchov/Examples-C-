6dd260211b7c80734808ca9fc57f25c9258ec384
Posuchov <135158331+Posuchov@users.noreply.github.com>
2023-06-12 17:41:58 +0200

    add new Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,6 +1,8 @@
 ﻿// работа с целыми числами//
 Console.Clear();
-int A = 31;
-int B = 5;
+int A = new Random().Next(1,10); //случайное число от 1,2,3,4, ... 9 //
+Console.WriteLine(A);
+int B = new Random().Next(1,10); //случайное число от 1,2,3,4, ... 9 //
+Console.WriteLine(B);
 int result = A+B;
 Console.WriteLine(result);
