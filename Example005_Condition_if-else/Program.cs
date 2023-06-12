6bad0f08d8c77c44924b59c2c9a0b7bc386ca7a8
Posuchov <135158331+Posuchov@users.noreply.github.com>
2023-06-12 20:44:52 +0200

    add Example005_Condition_if-else

---
+++ Example005_Condition_if-else/Program.cs
@@ -0,0 +1,13 @@
+﻿// использование if и else //
+Console.Write("Input User Name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "alexander")
+{
+    Console.WriteLine("YES! Es ist Alexander!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
